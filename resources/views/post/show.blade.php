8d6f955b8062919d99155271c82f37d33c73180b
Jasper Tan <Jasper@MacBook-Pro.local>
2021-12-18 10:05:17 +0800

    My first commit

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <img src="/storage/{{ $post->image }}" class="w-100">
+        </div>
+        <div class="col-4">
+            <h2>{{$user->name}}</h2>
+            <p> {{$post->caption}}</p>
+        </div>
+        <!--
+        <form action="{{ route('post.destroy',['post' => $post->id]) }}" enctype="multipart/form-data" method="post">
+            @csrf
+            {{ method_field("DELETE") }}
+            <div class ="form-group row">
+            <button type="submit" class="btn btn-primary">Delete</button>
+            </div>
+            </form>
+            
+                <form action="/example" method="POST">
+                @method('PUT')
+                    @csrf
+                    </form>
+            -->
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/profile', 'ProfileController@index');
+Route::get('/profile/create', 'ProfileController@create');
+Route::post('/profile', 'ProfileController@store')->name('profile.store');
+Route::get('/profile/edit', 'ProfileController@edit');
+Route::post('/profile/{id}/update', 'ProfileController@update')->name('profile.update');
+
+Route::get('/post/create', 'PostController@create');
+Route::post('/post', 'Postcontroller@store')->name('post.store');
+Route::get('/post/{postID}', 'Postcontroller@show');
+// Route::delete('/post', 'Postcontroller@destory');
+
+//Route::resource('post', App\Http\Controllers\PostController::class);
+Route::resource('resturant', App\Http\Controllers\ResturantController::class);
+Route::resource('review', App\Http\Controllers\ReviewController::class);
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
